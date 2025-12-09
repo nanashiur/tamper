2fb8f1acbc8472d4652d2201d2ff549d5f170ac1
nanashiur <mailid-general00@memoad.jp>
2025-12-09 11:48:46 +0900

    Update script version and main delay value

--- reload.js
+++ reload.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         ⏰ 40.40 (0-500 auto-info)
+// @name         ⏰ 40.50 (0-500 auto-info)
 // @namespace    http://tampermonkey.net/
-// @version      4.78
+// @version      4.79
 // @description  Auto-calculates info panel based on start time + max delay.
 // @match        https://reserve.tokyodisneyresort.jp/sp/hotel/list/*
 // @updateURL    https://raw.githubusercontent.com/nanashiur/tamper/refs/heads/main/reload.js
@@ -14,7 +14,7 @@
   'use strict';
 
   // ★★ Start time & delay → ここだけ変更すればOK
-  const main = { h: 10, m: 59, s: 40, ms: 400, max: 500 };
+  const main = { h: 10, m: 59, s: 40, ms: 500, max: 500 };
   const pre  = { h: 10, m: 52, s: 0,  ms: 0, max: 2000 };
 
   let trigMain = false, trigPre = false;
